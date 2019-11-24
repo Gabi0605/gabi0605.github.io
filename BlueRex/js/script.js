89f8efc2d5f2ce97f56d5c97b21a08ef684a8903
Regina <58051834+Gabi0605@users.noreply.github.com>
2019-11-24 14:01:10 +0300

    Add files via upload

--- BlueRex/js/script.js
+++ BlueRex/js/script.js
@@ -21,3 +21,55 @@ function trackScroll() {
       setTimeout(backToTop, 0);
     }
   }
+
+  $(document).ready(function(){
+    $("#nav").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
+
+
+
+// ------------------------------
+
+
+
+document.querySelectorAll('.modal-show').forEach(function(element) {
+  element.onclick = showModal;
+})
+
+document.querySelectorAll('.modal-close').forEach(function(element) {
+  element.onclick = closeModal;
+})
+
+document.querySelectorAll('.modal-wrap').forEach(function (element) {
+ element.onclick = closeModal;
+});
+
+function stop() {
+  event.stopPropagation();
+};
+
+document.querySelectorAll('.modal').forEach(function (element) {
+  element.onclick = stop;
+});
+
+function showModal() {
+  let modalId = this.dataset.modal;
+  document.querySelector(modalId).classList.remove('hide');
+  document.onkeydown = function(event) {
+      if (event.keyCode == 27) closeModal();
+  }
+  
+}
+
+function closeModal() {
+  document.querySelectorAll('.modal-wrap').forEach(function(element) {
+      element.classList.add('hide');
+  })
+  document.onkeydown = null;
+  document.querySelector('.form-slider').style.marginLeft = 0;
+}
