9e75454733e7e101056a0ae706f83b4fd1c2aa17
Regina <regina-golik@yandex.ru>
2020-02-01 19:59:10 +0300

    Добавила php

--- Irvas/js/main.js
+++ Irvas/js/main.js
@@ -1,23 +1,4 @@
 
-    
-    function call() {
-      var msg   = $('form').serialize();
-      var call = $('.modal-call');
-       $.ajax({
-         type: 'POST',
-         url: 'formx.php', //обращаемся к обработчику
-         data: msg,
-        success: function(data) {  //в случае успеха выводим результаты в div "results"
-            
-         },
-         error:  function(xhr, str){ //ошибка выводит соответствующее сообщение 
-	    alert('Возникла ошибка: ' + xhr.responseCode);
-         }
-       });
-   }
-
-
-
 document.querySelectorAll('.btn-collapse').forEach(function(element) {
     element.onclick = showCollapse;
 })
@@ -88,73 +69,4 @@ function showModal() {
         event.stopPropagation()
     }
 }
-
-(function( $ ){
-
-    $(function() {
-    
-      $('.form').each(function(){
-        // Объявляем переменные (форма и кнопка отправки)
-        var form = $(this),
-            btn = form.find('button');
-            
-        // Добавляем каждому проверяемому полю, указание что поле пустое
-        form.find('input').addClass('empty_field');
-    
-        // Функция проверки полей формы
-        function checkInput(){
-          form.find('input').each(function(){
-            if($(this).val() != ''){
-              // Если поле не пустое удаляем класс-указание
-            $(this).removeClass('empty_field');
-            } else {
-              // Если поле пустое добавляем класс-указание
-            $(this).addClass('empty_field');
-            }
-          });
-        }
-    
-        // Функция подсветки незаполненных полей
-        function lightEmpty(){
-          form.find('.empty_field').css({'border-color':'#d8512d'});
-          // Через полсекунды удаляем подсветку
-          setTimeout(function(){
-            form.find('.empty_field').removeAttr('style');
-          },500);
-        }
-    
-        // Проверка в режиме реального времени
-        setInterval(function(){
-          // Запускаем функцию проверки полей на заполненность
-          checkInput();
-          // Считаем к-во незаполненных полей
-          var sizeEmpty = form.find('.empty_field').size();
-          // Вешаем условие-тригер на кнопку отправки формы
-          if(sizeEmpty > 0){
-            if(btn.hasClass('disabled')){
-              return false
-            } else {
-              btn.addClass('disabled')
-            }
-          } else {
-            btn.removeClass('disabled')
-          }
-        },500);
-    
-        // Событие клика по кнопке отправить
-        btn.click(function(){
-          if($(this).hasClass('disabled')){
-            // подсвечиваем незаполненные поля и форму не отправляем, если есть незаполненные поля
-            lightEmpty();
-            return false
-          } else {
-            // Все хорошо, все заполнено, отправляем форму
-            form.submit();
-          }
-        });
-      });
-    });
-    
-    })( jQuery );
-    
 
